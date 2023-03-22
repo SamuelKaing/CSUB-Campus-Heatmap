e1dfac9ec73d8ad67c347b23f7cd8217b3709d12
Samuel Kaing <78284945+SamuelKaing@users.noreply.github.com>
2023-03-22 09:19:25 -0700

    Added new table

--- schedules.sql
+++ schedules.sql
@@ -1,10 +1,25 @@
+DROP TABLE IF EXISTS 'Buildings';
 CREATE TABLE Buildings (
-    BuildingID integer primary key autoincrement,
+    BuildingID integer primary key auto_increment,
     BuildingName text not null,
     ClosestLot text not null
 );
 
+DROP TABLE IF EXISTS 'Classes';
 CREATE TABLE Classes (
-    ClassID integer primary key autoincrement,
-    StartTime text not null
+    ClassNumber integer primary key,
+    StartTime text not null,
+    EndTime text not null,
+    StartDate text not null,
+    EndDate text not null,
+    TotalEnrolled integer not null
+);
+
+DROP TABLE IF EXISTS 'Inside';
+CREATE TABLE Inside (
+    ClassNumber integer not null,
+    BuildingID integer not null,
+    foreign key (ClassNumber) references Classes(ClassNumber),
+    foreign key (BuildingID) references Buildings(BuildingID),
+    primary key (ClassNumber, BuildingID)
 );
