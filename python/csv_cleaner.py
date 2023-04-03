b964b2fd5d2394f5623056355498a89fc5925a87
Samuel Kaing <sam_kaing@yahoo.com>
2023-04-02 22:10:40 -0700

    Inserts matches new table

--- python/csv_cleaner.py
+++ python/csv_cleaner.py
@@ -16,12 +16,13 @@ for column in unwanted:
     df = df.drop(columns=[column])
 
 # Filters Rows
-df = df[df['Class Stat'] == 'Active']
+df = df[df['Class Stat'] == 'Active']       # delete cancelled classes
+df = df.drop(columns=['Class Stat'])        # column not needed anymore
 df = df[df['Building'] != 'WEB']
 df = df[df['Building'] != 'OFFCMP']
 df = df[df['Building'] != '']
 df = df.dropna(subset=['Building'])
-df = df.drop(columns=['Class Stat'])
+df = df.drop_duplicates()
 
 df.to_csv('cleaned_classes.csv')
 
@@ -30,7 +31,18 @@ building_inserts = """"""
 inside_inserts = """"""
 buildings = []
 for row in df.itertuples():
-    class_query = """INSERT INTO Classes (ClassNumber, TotalEnrolled, StartDate, EndDate, StartTime, EndTime) VALUES ({}, {}, '{}', '{}', '{}', '{}');\n""".format(row[1], row[2], row[3], row[4], row[5], row[6])
+    days = []
+    
+    # checks what days class occurs on 
+    for index in range(5):
+        #check row[index], if not empty, append true, else append false
+        if pd.isna(row[index+7]):
+            days.append(False)
+        else:
+            days.append(True)
+
+    class_query = """INSERT INTO Classes (ClassID, ClassNumber, TotalEnrolled, StartDate, EndDate, StartTime, EndTime, Mon, Tues, Wed, Thur, Fri) 
+    VALUES ({}, {}, {}, '{}', '{}', '{}', '{}', {}, {}, {}, {}, {});\n""".format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], days[0], days[1], days[2], days[3], days[4])
     class_inserts += class_query
     
     # only adds unique buildings
@@ -39,21 +51,13 @@ for row in df.itertuples():
         building_inserts += building_query
         buildings.append(row[14])
 
-    # since buildings are only appended to the buildings array and concatentated to the buidling_inserts string by order of appearance in the df, 
-    # the first building to be appended has the building_id of 1, the second building to be appended has the building_id of 2, and so forth
-    # so when looking at the class # and building of a row, we can get the id of the building by searching for its index+1 within the buildings array
-    # with the class # and the building_id found, we can create the insert statement for the 'Is In' relational table
-
+    # relational table queries
     for index, building in enumerate(buildings):
         if row[14] is building:
-            inside_query = """INSERT INTO Inside (ClassNumber, BuildingID) VALUES ({}, {});\n""".format(row[1], index+1)
+            inside_query = """INSERT INTO Inside (ClassID, ClassNumber, BuildingID) VALUES ({}, {}, {});\n""".format(row[0], row[1], index+1)
             inside_inserts += inside_query
 
-#print(class_inserts)
-#print(building_inserts)
-#print(inside_inserts)
 all_inserts = class_inserts + building_inserts + inside_inserts
-#print(all_inserts)
 
 with open('../results.sql', 'w') as file:
   file.write(all_inserts)
